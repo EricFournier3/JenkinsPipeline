f1611858ec2f729ac4b932d6f496f8dee578c42f
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2019-07-11 12:00:31 -0400

    Eric Fournier 2019-07-11

--- InitGenomicPipeline.sh
+++ InitGenomicPipeline.sh
@@ -5,6 +5,9 @@ Eric Fournier 2019-07-09
 
 HEADER
 
+echo "******************************* TESTING *******************"
+
+
 PASS_FILE="/home/foueri01@inspq.qc.ca/pass.txt"
 
 source "/home/foueri01@inspq.qc.ca/GitScript/Jenkins/SetPath.sh"
